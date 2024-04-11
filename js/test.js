c8d304314f0568cc206da4fc53b069a7887702f6
revan <rvanliyev55@gmail.com>
2024-04-11 18:00:30 +0400

    Registration system is over

--- js/test.js
+++ js/test.js
@@ -1,40 +1,18 @@
-let i = 0;
+if (Boolean(localStorage.getItem("usersData")) == false) {
+  localStorage.setItem("usersData", "*");
+}
 function signIn() {
-  let signInUserName = document.getElementById("signInUserName").value;
-  let signInUserSurname = document.getElementById("signInUserSurname").value;
   let signInUserEmail = document.getElementById("signInUserEmail").value;
   let signInUserPassword = document.getElementById("signInUserPassword").value;
-  let user = {
-    key: i,
-    Name: signInUserName,
-    Surname: signInUserSurname,
-    Email: signInUserEmail,
-    Password: signInUserPassword,
-  };
-  localStorage.setItem(i, JSON.stringify(user));
-  i++;
+  localStorage.setItem(
+    "usersData",
+    Array(
+      String(Array(localStorage.getItem("usersData"))).replace(
+        "*",
+        `${signInUserEmail} : ${signInUserPassword},*`
+      )
+    )
+  );
 }
-function login() {
-  let loginUserEmail = document.getElementById("loginUserEmail").value;
-  let loginUserPassword = document.getElementById("loginUserPassword").value;
-  let l;
-  for (i = 0; i < localStorage.length; i++) {
-    let userEmail = Object.entries(JSON.parse(localStorage.getItem(i)))[3][1];
-    let userPassword = Object.entries(
-      JSON.parse(localStorage.getItem(i))
-    )[4][1];
-    console.log(i);
-    if (
-      userEmail == loginUserEmail &&
-      userPassword == loginUserPassword &&
-      i <= localStorage.length
-    ) {
-      l = true;
-    }
-  }
-  if (l == true) {
-    alert("login succesful");
-  } else {
-    alert("login failed");
-  }
-}
+
+function login() {}
