d31ed28a37b3a65923bb77734fc0bd16665e1921
revan <rvanliyev55@gmail.com>
2024-04-13 18:18:09 +0400

    js modified

--- js/test.js
+++ js/test.js
@@ -1,30 +1,50 @@
 if (Boolean(localStorage.getItem("usersData")) == false) {
   localStorage.setItem("usersData", "*");
 }
+// if (signInUserEmail.length > 0){
+//   document.getElementById("signInUserEmail").style.backgroundColor = "transparent"
+// }
+
+// if (signInUserPassword.length > 0){
+//   document.getElementById("signInUserPassword").style.backgroundColor = "transparent"
+// }
 function signIn() {
   let signInUserEmail = document.getElementById("signInUserEmail").value;
   let signInUserPassword = document.getElementById("signInUserPassword").value;
-  localStorage.setItem(
-    "usersData",
-    Array(
-      String(Array(localStorage.getItem("usersData"))).replace(
-        "*",
-        `${signInUserEmail} : ${signInUserPassword},*`
+  if (signInUserPassword.length > 0 && signInUserEmail.length > 0) {
+    localStorage.setItem(
+      "usersData",
+      Array(
+        String(Array(localStorage.getItem("usersData"))).replace(
+          "*",
+          `${signInUserEmail} : ${signInUserPassword},*`
+        )
       )
-    )
-  );
+    );
+  } else if (signInUserEmail.length == 0) {
+    document.getElementById("signInUserEmail").style.backgroundColor = "red";
+  } else if (signInUserPassword.length == 0) {
+    document.getElementById("signInUserPassword").style.backgroundColor = "red";
+  }
 }
 
 function login() {
   let logUserEmail = document.getElementById("logUserEmail").value;
   let logUserPassword = document.getElementById("logUserPassword").value;
-  if (localStorage.getItem("usersData").search(logUserEmail) >= 0) {
-    if (localStorage.getItem("usersData").search(logUserPassword) >= 0) {
-      alert("Login Succesful!");
+  console.log(document.getElementById("logUserEmail").value);
+  if (logUserEmail.length > 0 && logUserPassword.length > 0) {
+    if (localStorage.getItem("usersData").search(logUserEmail) >= 0) {
+      if (localStorage.getItem("usersData").search(logUserPassword) >= 0) {
+        alert("Login Succesful!");
+      } else {
+        alert("Login Failed!");
+      }
     } else {
       alert("Login Failed!");
     }
-  } else {
-    alert("Login Failed!");
+  } else if (logUserEmail.length == 0) {
+    document.getElementById("logUserEmail").style.backgroundColor = "red";
+  } else if (logUserPassword == 0) {
+    document.getElementById("logUserPassword").style.backgroundColor = "red";
   }
 }
